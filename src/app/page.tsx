5f8b888c20b6abc01016f2f93f0dc5960d2cf293
Rodrigo Pontes Moreira <111535306+RpMoreira98@users.noreply.github.com>
2025-05-28 17:23:30 -0300

    feat: css styling on cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { CardComponents } from "@/components/cardComponents/cardComponents";
 const Home = () => {
   return (
     <div className="container p-6 lg:p-20 mx-auto">
-      <h1 className="text-4xl font-bold text-rose-950 mb-6">Deserts</h1>
+      <h1 className="text-4xl font-extrabold text-[#260F08] mb-6">Deserts</h1>
       <div className="grid grid-cols-1 lg:grid-cols-70-30 gap-8 place-items-start">
         <CardComponents />
       </div>
--- src/components/cardList/cardList.tsx
+++ src/components/cardList/cardList.tsx
@@ -10,16 +10,34 @@ export const CardList = ({ cardList }: CardListCandy) => {
   const addToCart = useCartStore((state) => state.addToCart);
   return (
     <div className="">
-      <Image
-        src={cardList.image}
-        alt={cardList.title}
-        width={250}
-        height={200}
-      />
-      <h3>{cardList.title}</h3>
-      <p>{cardList.text}</p>
-      <p>${cardList.value.toFixed(2)}</p>
-      <button onClick={() => addToCart(cardList)}> Add to Cart</button>
+      <div className="relative mb-8">
+        <Image
+          src={cardList.image}
+          alt={cardList.title}
+          width={250}
+          height={200}
+          className="rounded-lg"
+        />
+        <button
+          onClick={() => addToCart(cardList)}
+          className="absolute py-2 px-4 gap-2 text-[#260F08] rounded-full bottom-[-1.2rem] cursor-pointer flex justify-between items-center bg-white left-2/4 -translate-x-1/2 min-w-36 border-[#260F08] text-sm font-semibold border"
+        >
+          <Image
+            src={"/icon-add-to-cart.svg"}
+            alt="addTocart"
+            width={20}
+            height={20}
+          />{" "}
+          Add to Cart
+        </button>
+      </div>
+      <div className="">
+        <h3 className="text-[#260F08] font-normal text-sm">{cardList.title}</h3>
+        <p className="text-[#260F08] font-extrabold text-xl">{cardList.text}</p>
+        <p className="text-red-500 font-normal mb-4">
+          ${cardList.value.toFixed(2)}
+        </p>
+      </div>
     </div>
   );
 };
