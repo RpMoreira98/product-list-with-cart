fd006018472513c90fd935f0d6e118b0d353c94a
Rodrigo Pontes Moreira <111535306+RpMoreira98@users.noreply.github.com>
2025-05-29 16:20:33 -0300

    feat: added items to cart

--- src/components/cardComponents/cardComponents.tsx
+++ src/components/cardComponents/cardComponents.tsx
@@ -1,12 +1,18 @@
 import { Candy } from "@/data/Candy";
 import { CardList } from "../cardList/cardList";
+import { CardConfirm } from "../cardConfirm/cardConfirm";
 
 export const CardComponents = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-      {Candy.map((candy) => (
-        <CardList key={candy.id} cardList={candy} />
-      ))}
+    <div className="flex gap-[6rem]">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {Candy.map((candy) => (
+          <CardList key={candy.id} cardList={candy} />
+        ))}
+      </div>
+      <div className="">
+        <CardConfirm />
+      </div>
     </div>
   );
 };
---
+++ src/components/cardConfirm/cardConfirm.tsx
@@ -0,0 +1,74 @@
+import { useCartStore } from "@/store/stores";
+import Image from "next/image";
+
+export const CardConfirm = () => {
+  const { cart, removeFromCart } = useCartStore();
+
+  const total = cart.reduce((acc, item) => acc + item.value * item.amount, 0);
+  return (
+    <div className="bg-white w-[400px] p-4 py-6 rounded-sm">
+      <h1 className="text-red-500 font-extrabold text-xl">
+        Your Cart ({cart.length})
+      </h1>
+      {cart.length === 0 ? (
+        <div className="flex flex-col items-center gap-3 justify-center">
+          <Image
+            src={"/illustration-empty-cart.svg"}
+            alt="emptyCart"
+            width={128}
+            height={128}
+          />
+          <span className="text-rose-950 text-center text-sm">
+            Your added items will appear here
+          </span>
+        </div>
+      ) : (
+        <div>
+          <ul className="space-6">
+            {cart.map((item) => (
+              <li
+                key={item.id}
+                className="flex items-center justify-between gap-4 rounded-lg py-4 border-b border-rose-100"
+              >
+                <div className="flex flex-col items-start">
+                  <h1 className="text-black text-sm font-bold">{item.text}</h1>
+                  <div className="flex justify-between gap-3 text-rose-500">
+                    <div className="flex gap-2">
+                      <span className="text-xs">{item.amount}x</span>
+                      <span className="text-xs">
+                        $ {(item.value * item.amount).toFixed(2)}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <button
+                    className="border p-1 rounded-full cursor-pointer"
+                    onClick={() => removeFromCart(item.id)}
+                  >
+                    <Image
+                      src={"/icon-remove-item.svg"}
+                      alt={item.image}
+                      width={10}
+                      height={10}
+                    />
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+          <div className="flex justify-between mt-3 text-black">
+            <h3 className="font-normal">Order total</h3>
+            <span className="font-extrabold text-xl">${total.toFixed(2)}</span>
+          </div>
+          <div>
+            <h3></h3>
+          </div>
+          <button className="w-full cursor-pointer px-2 py-2 mt-3 font-bold bg-red-700 hover:bg-red-800 rounded-full">
+            Confirme Order
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
