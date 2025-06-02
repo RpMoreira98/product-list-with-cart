f17ba0ef5db1e1ecae0f26c8ab6dd7aa03ae120e
Rodrigo Pontes Moreira <111535306+RpMoreira98@users.noreply.github.com>
2025-06-02 09:04:31 -0300

    feat: added final modal style

--- src/components/modalConfimation/modalConfirmation.tsx
+++ src/components/modalConfimation/modalConfirmation.tsx
@@ -16,42 +16,61 @@ export const ModalConfimation = ({ isOpen, onClose }: Modal) => {
   };
 
   return (
-    <div className="fixed inset-0 bg-zinc-50 bg-opacity-50 flex flex-col justify-center items-center z-50">
-      <div className="bg-white rounded-lg p-8 w-[90%] max-w-md text-center">
-        <div className="flex justify-center mb-4">
-          <div className="w-8 h-8 border-2 border-green-500 rounded-full flex items-center justify-center"></div>
+    <div className="fixed inset-0 bg-[#e2e0de42] flex flex-col justify-center items-center z-50">
+      <div className="bg-white rounded-lg w-[22rem] p-8">
+        <div className="bg-white pb-3 w-[90%] max-w-md text-center">
+          <div className="w-10 h-10 border-2 border-green-500 rounded-full flex items-center justify-center">
+            <Image
+              src={"/icons-check.svg"}
+              alt="check-icon"
+              height={20}
+              width={25}
+            />
+          </div>
         </div>
-      </div>
-      <h2 className="text-2xl font-bold text-[#260f08] mb-2">
-        Order Confirmed
-      </h2>
-      <p className="text-rose-300">We hope you enjoy your food!</p>
-      <div className="bg-rose-100 p-4 rounded-lg mb-6">
-        {cart.map((item) => (
-          <div key={item.id} className="flex justify-between items-center mb-2">
-            <div className="flex items-center gap-2">
-              <Image
-                src={item.image}
-                alt={item.title}
-                width={30}
-                height={20}
-                className="rounded"
-              />
-              <p className="font-semibold text-sm">{item.text}</p>
-              <p className="text-red-500 text-sm">
-                {item.amount}X{" "}
-                <span className="text-rose-300">{item.value.toFixed(2)}</span>
+        <h2 className="text-3xl font-extrabold text-[#260f08] mb-2">
+          Order Confirmed
+        </h2>
+        <p className="text-rose-300 text-xs pt-2">
+          We hope you enjoy your food!
+        </p>
+        <div className="grid mb-2 mt-2 bg-gray-100 p-4 gap-2 max-h-60 overflow-y-auto rounded-lg">
+          {cart.map((item) => (
+            <div
+              key={item.id}
+              className="flex justify-between items-center border-b border-rose-100 pb-2 mb-2"
+            >
+              <div className="flex items-center gap-2">
+                <div>
+                  <Image
+                    src={item.image}
+                    alt={item.title}
+                    width={30}
+                    height={20}
+                    className="rounded"
+                  />
+                </div>
+                <div>
+                  <p className="font-semibold text-[#260f08] text-sm">
+                    {item.text}
+                  </p>
+                  <p className="text-red-500 font-bold text-sm">
+                    {item.amount}x{" "}
+                    <span className="text-rose-300">
+                      ${item.value.toFixed(2)}
+                    </span>
+                  </p>
+                </div>
+              </div>
+              <p className="font-semibold text-[#260f08]">
+                ${(item.value * item.amount).toFixed(2)}
               </p>
             </div>
-            <p className="font-semibold text-[#260f08]">
-              ${(item.value * item.amount).toFixed(2)}
-            </p>
-          </div>
-        ))}
-
+          ))}
+        </div>
         <button
           onClick={handleSubmitModal}
-          className="bg-[#c73620] text-white py-2 px-6 rounded-full font-semibold hover:bg-red-700"
+          className="bg-[#c73620] text-white mt-4 py-2 px-6 w-full rounded-full font-semibold hover:bg-red-700"
         >
           Submit new Order
         </button>
